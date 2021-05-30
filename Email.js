743ce8db65273cf8f735e717fb8d369b3c28de0e
Abhishek <ABHISHEKKUMAR998>
2021-05-30 23:45:25 +0530

    Email with regex

--- Email.js
+++ Email.js
@@ -1,6 +1,16 @@
 let PinRegex = RegExp('^[a-z][0-9a-z_+-]*\\.?[0-9a-z_+-]+@\\w+(\\.[a-z]{2,}){1,2}$');
+
+//UC-1: Check Initial Part
+let email_1 = "abc";
+check_validity(email_1);
+
+//UC-2: Chect @ and Part Latter it.
 let email_2 = "abc@bridgelab";
 check_validity(email_2);
+
+//UC-3: Add . after Bridgelab
+let email_3 = "abc.@bridgelab.co";
+check_validity(email_3);
 
 
 function check_validity(sample) {
