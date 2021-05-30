a258ecd699656a70ca26b327ed81c8bfeab39682
Abhishek <ABHISHEKKUMAR998>
2021-05-30 23:44:13 +0530

    Email with regex

--- Email.js
+++ Email.js
@@ -1,6 +1,9 @@
-let email_1 = "abc";
-check_validity(email_1);
+let PinRegex = RegExp('^[a-z][0-9a-z_+-]*\\.?[0-9a-z_+-]+@\\w+(\\.[a-z]{2,}){1,2}$');
+let email_2 = "abc@bridgelab";
+check_validity(email_2);
+
 
 function check_validity(sample) {
     let output = PinRegex.test(sample);
     console.log(sample + " \nEmail is Valid: " + output);
+}
